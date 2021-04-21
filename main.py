9900cafe74e0e899aba4af24acad5ec64fbf7244
asazas <asazas0@hotmail.com>
2021-04-21 17:40:36 +0200

    discord.ext.commands, mejorado crear seeds

--- main.py
+++ main.py
@@ -2,28 +2,18 @@ from configparser import ConfigParser
 
 import yaml
 
-import seedgen
+from seedgen import Seedgen
 
-import discord  # pip install discord.py
+from discord.ext import commands  # pip install discord.py
 
-class MyClient(discord.Client):
+class BolasBot(commands.Bot):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
-
-    async def on_message(self, message):
-        print('Message from {0.author}: {0.content}'.format(message))
-
-        if message.content == "!crea-seed" and message.attachments:
-            print("Es un crea seed")
-            settings_file = message.attachments[0]
-            my_file = await settings_file.read()
-            my_yaml = yaml.load(my_file, Loader=yaml.FullLoader)
-            seed = await seedgen.crea_seed_con_settings(my_yaml)
-            await message.reply(seed.url, mention_author=False)
 
 
 if __name__ == "__main__":
     config = ConfigParser()
     config.read('config.ini')
-    client = MyClient()
-    client.run(config['auth']['token'])
+    bot = BolasBot(command_prefix=config['commands']['prefix'])
+    bot.add_cog(Seedgen(bot))
+    bot.run(config['auth']['token'])
--- seedgen.py
+++ seedgen.py
@@ -2,11 +2,33 @@ import yaml     # pip install pyyaml
 
 import pyz3r    # pip install pyz3r
 
-async def crea_seed_con_settings(my_settings):
-    seed = await pyz3r.alttpr(settings=my_settings['settings'])
-    return seed
+import discord
 
-if __name__ == "__main__":
-    with open("rando-settings/tournament.yaml") as settings_file:
-        settings = yaml.load(settings_file, Loader=yaml.FullLoader)
-        print(settings)
+from discord.ext import commands
+
+class Seedgen(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+   
+    @commands.command()
+    async def crea_seed(self, ctx, preset: str=""):
+
+        my_settings = ""
+
+        if ctx.message.attachments:
+            my_settings = await (ctx.message.attachments[0]).read()
+        
+        if preset:
+            with open("rando-settings/{}.yaml".format(preset), "r") as settings_file:
+                my_settings = settings_file.read()
+        
+        settings_yaml = yaml.load(my_settings, Loader=yaml.FullLoader)
+        seed = await pyz3r.alttpr(settings=settings_yaml['settings'])
+        await ctx.reply(seed.url, mention_author=False)
+
+    
+    @crea_seed.error
+    async def crea_seed_error(self, ctx, error):
+        err_file = discord.File("media/error.png")
+        await ctx.reply("Se ha producido un error.", mention_author=False, file=err_file)
+        
